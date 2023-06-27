0180cd367ad3be1f310a68cee71b9547dcabee96
ccp84 <ccp84@icloud.com>
2023-06-27 09:43:57 +0000

    build

--- src/Pages/Welcome.js
+++ src/Pages/Welcome.js
@@ -27,20 +27,26 @@ const Welcome = () => {
 					<Alert variant="info">
 						<Card.Title>Sports Massage in Malvern</Card.Title>
 					</Alert>
-					<Card.Text>
-						Hi! I'm Ian, I've recently qualified as a Level 3 Sports
-						Massage Therapist. After 30+ years of working in the
-						packaging industry, it's time for a change of direction!
-						I took up running about 9 years ago and have completed a
-						number of marathons, half marathons, mud runs and
-						obstacle course races in that time. I'm also a member of
-						The Malvern Joggers, who I've run with for the last 5 or
-						6 years. Still working full time at the moment and
-						fitting the Sports Massage in around that, so I'm
-						currently available for massage after 17:00 Mon - Thurs,
-						after 12:00 on Friday and all day Saturday and Sunday
-						races permitted.
-					</Card.Text>
+					<Card>
+						<Card.Body>
+							<Card.Text>
+								Hi! I'm Ian, I've recently qualified as a Level
+								3 Sports Massage Therapist. After 30+ years of
+								working in the packaging industry, it's time for
+								a change of direction! I took up running about 9
+								years ago and have completed a number of
+								marathons, half marathons, mud runs and obstacle
+								course races in that time. I'm also a member of
+								The Malvern Joggers, who I've run with for the
+								last 5 or 6 years. Still working full time at
+								the moment and fitting the Sports Massage in
+								around that, so I'm currently available for
+								massage after 17:00 Mon - Thurs, after 12:00 on
+								Friday and all day Saturday and Sunday races
+								permitted.
+							</Card.Text>
+						</Card.Body>
+					</Card>
 				</Card.Body>
 			</Card>
 		</>
